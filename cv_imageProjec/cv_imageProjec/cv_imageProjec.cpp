76882611dcdfb34a584df12582f560d441c1dfed
Kavy Chou <kavychou@moldex3d.com>
2023-11-26 16:04:25 +0800

    2023/11/26 course

---
+++ cv_imageProjec/cv_imageProjec/cv_imageProjec.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+int main() {
+    // Greeting
+    std::cout << "Hello OpenCV World!\n";
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+    
+    // Define
+    unsigned int index{ 0 };
+    
+    std::cout << "Please input your test case index: ";
+    std::cin >> index;
+
+    switch (index) {
+    case 0: {
+        // Test case 0: 
+        std::string test_image_path;
+        cv::Mat test_image;
+        std::cout << "Please input test image path: ";
+        std::cin >> test_image_path;
+        std::cout << "Your image path is: " << test_image_path << std::endl;
+
+        try {
+            test_image = cv::imread(test_image_path);
+            cv::imshow("Selected file image", test_image);
+            cv::waitKey(0);
+            cv::destroyAllWindows();
+        }
+        catch (std::exception e) {
+            std::cerr << "Exception get: \t" << e.what() << std::endl;
+        }
+    }
+    default:
+        break;
+    }
+
+    
+}
